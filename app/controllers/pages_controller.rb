d75cb66e86d5e2b2aa793758d3f53e261c22d201
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2010-03-13 18:18:47 -0200

    When logged in, root_path redirects to events_path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  skip_before_filter :authenticate_user!
+  before_filter :redirect_logged_in_users
+
   def root
   end
+
+  def redirect_logged_in_users
+    redirect_to events_path if user_signed_in?
+  end
+  hide_action :redirect_logged_in_users
 end
