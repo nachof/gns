9caaff75e03873245bfd57391cacffe5a5ac7ddb
Nacho Facello <nacho@nucleartesuji.com>
2010-04-08 21:06:55 -0300

    Preload the invitess in the list

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-  $("#event_invitee_ids").tokenInput("/users.json", { hintText: "Escribí el nombre o email de la persona", noResultsText: "Sin resultados :(", searchingText: "Buscando..." })
+  var preloadData = eval($("#event_invitee_ids").attr("data-preload"));
+  $("#event_invitee_ids").tokenInput("/users.json", { hintText: "Escribí el nombre o email de la persona", noResultsText: "Sin resultados :(", searchingText: "Buscando...", prePopulate: preloadData })
 });
